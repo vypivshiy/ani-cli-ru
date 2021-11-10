c7bbe6cd1180d0e595c39239551f0d2ab66cf624
Georgiy <bomb3r98@gmail.com>
2021-11-10 13:50:38 +0300

    update tests

--- tests.py
+++ tests.py
@@ -1,59 +1,58 @@
 import unittest
 
-import anicli_ru as anicli
+from anicli_ru import Anime, ListObj
 
 
 class TestRequests(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        cls.anime = anicli.Anime()
+        cls.anime = Anime()
 
     def test_get_ongoings(self):
         """Test get ongoings list"""
-        ongoings = self.anime.get_ongoing()
-        self.assertIsInstance(ongoings, list)
+        ongoings = self.anime.ongoing()
+        self.assertIsInstance(ongoings, ListObj)
         self.assertGreater(len(ongoings), 0)
 
     def test_search_1(self):
         """Test get anime list"""
-        results = self.anime.search("Вайолет")
+        results = self.anime.search("Violet Evergarden")
         self.assertEqual(len(results), 4)
+        self.assertIsInstance(results, ListObj)
 
     def test_search_2(self):
         """Test get banned title in Russia Federation"""
-        self.anime.search("Эльфийская песнь")  # title, who have bigger than 1 episode and banned in Russia
-        episodes = self.anime.parse_episodes_count(0)
-        self.assertEqual(episodes, 13)
-        self.anime.parse_series()
-        self.assertEqual(len(self.anime._episodes), 0)
+        results = self.anime.search("Эльфийская песнь")  # title, who have bigger than 1 episode and banned in Russia
+        episodes = self.anime.episodes(results[0])
+        self.assertEqual(len(episodes), 0)
 
     def test_search_3(self):
         """Test normal case search and get episode"""
-        self.anime.search("Джо")  # Jo'Jo Adventure
-        episodes = self.anime.parse_episodes_count(5)  # chapter 1
-        self.assertEqual(episodes, 26)
-        self.anime.parse_series()
-        choose = self.anime.choose_episode(0)
-        self.assertEqual(choose[0]["dub"], "AniDUB")
-        self.assertTrue("sibnet" in choose[0]["player"][0])  # sibnet player available
+        results = self.anime.search("Джо")  # Jo'Jo Adventures
+        episodes = results.choose(6).episodes()  # chapter 1
+        self.assertEqual(len(episodes), 26)
+        choose = episodes[0]
+        players = choose.player()
+        self.assertEqual(players[0].dub_name, "AniDUB")
+        self.assertTrue("sibnet" in players[0].url)  # sibnet player available
 
     def test_search_4(self):
         """Test get ongoing series"""
-        ongoings = self.anime.get_ongoing()
+        ongoings = self.anime.ongoing()
+        self.assertGreater(len(ongoings), 0)
         if len(ongoings) > 0:
-            episodes_count = self.anime.parse_episodes_count(1)
-            self.assertIsInstance(episodes_count, int)
-            self.anime.parse_series()
-            self.assertGreater(len(self.anime._episodes), 0)
+            episodes = self.anime.episodes(ongoings.choose(1))
+            self.assertIsInstance(episodes, ListObj)
+            self.assertGreater(len(episodes), 0)
         else:
             self.skipTest("Cannot get ongoings in your country or ip address")
 
     def test_search_5(self):
         """Test get anime episodes, but they have only an unsupported kodik player :( """
-        self.anime.search("lain")  # experiments lain
-        episodes = self.anime.parse_episodes_count(0)
-        self.assertEqual(episodes, 13)
-        self.anime.parse_series()
-        choose = self.anime.choose_episode(12)
-        self.assertIsInstance(choose, list)
-        self.assertEqual(len(choose), 0)
+        results = self.anime.search("lain")  # experiments lain
+        episodes = results[0].episodes()
+        self.assertEqual(len(episodes), 13)
+        players = episodes[12].player()
+        self.assertIsInstance(players, ListObj)
+        self.assertEqual(len(players), 0)
+
