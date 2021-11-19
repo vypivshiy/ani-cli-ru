41384b0dfadcab2eb24d4df7705202d2c2c39cd2
Georgiy <bomb3r98@gmail.com>
2021-11-19 21:12:09 +0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from setuptools import setup
+
+from anicli_ru import __version__
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+setup(
+    name='anicli-ru',
+    version=__version__,
+    packages=['anicli_ru'],
+    url='https://github.com/vypivshiy/ani-cli-ru',
+    license='GPL-3',
+    author='georgiy',
+    author_email='',
+    description='anime-ru grabber video api',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=['requests'],
+)
