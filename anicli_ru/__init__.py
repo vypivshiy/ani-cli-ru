1a2ffa84c33ce1ab657e89df67165b9c1cd8ef72
Georgiy <bomb3r98@gmail.com>
2022-02-04 13:57:27 +0300

    add update method

--- anicli_ru/__init__.py
+++ anicli_ru/__init__.py
@@ -1,2 +1,15 @@
-from anicli_ru.api import Anime
+from anicli_ru.loader import all_extractors, import_extractor
 __version__ = "3.0.3"
+
+
+def check_update():
+    """check last version package"""
+    import requests
+    import re
+    r = requests.get("https://raw.githubusercontent.com/vypivshiy/ani-cli-ru/master/anicli_ru/__init__.py")
+    version, = re.findall(r'__version__ = "([\d.]+)"', r.text)
+    return version
+
+
+if __name__ == '__main__':
+    print(check_update())
