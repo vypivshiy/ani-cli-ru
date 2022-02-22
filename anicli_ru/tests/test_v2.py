e77a28c4dea91506f001e774d317adaa9b3f1399
Georgiy <bomb3r98@gmail.com>
2022-02-22 17:10:04 +0300

    remove old tests.py, change unittest to pytest

--- anicli_ru/tests/test_v2.py
+++ anicli_ru/tests/test_v2.py
@@ -1,11 +1,13 @@
 """
 Базовый тест для тестирования парсеров. Тест может проходить довольно долго
-(от 5-30 секунд за один парсер), так как посылаются запросы на реальный сайт, без подмены ответов.
+(от 5-30 секунд за один парсер), так как посылаются запросы на реальный сайт, без применения monkey path.
 Это гарантирует работоспособность парсера как минимум на момент релиза.
 
-В идеальных условиях, парсер должен проходить **все тесты** без переопределения тестов.
+В идеальных условиях, парсер должен проходить **все тесты** без переопределения.
+Если это не так, то переопределите в классе Anime словарь _TESTS
 """
-
+# TODO добавить аргументы для теста одного выбранного парсера
+import warnings
 from sys import modules as sys_modules
 
 from anicli_ru import loader
@@ -26,64 +28,75 @@ for m in ["anicli_ru.extractors." + m for m in extractors]:
 ANIME_HTTP_ALL = [p.Anime() for p in IMPORTED_MODULES]
 
 
-@pytest.mark.parametrize("anime,q", [(p, "experiments lain") for p in ANIME_HTTP_ALL])
-def test_get_search_result(anime: BaseAnimeHTTP, q: str):
+def is_video(url):
+    return ".m3u8" in url or ".mp4" in url or "sibnet" in url
+
+
+@pytest.mark.parametrize("anime", ANIME_HTTP_ALL)
+def test_get_search_result(anime: BaseAnimeHTTP):
     """Тест, по поиску тайтла по строке.
 
     По умолчанию ищет по запросу 'experiments lain' (Эксперименты Лэйн).
     Этот тайтл должен быть гарантированно на сайте. Если это не так и этот парсер должен быть обязательно
-    в этом скрипте, то переопределите этот тест.
-
-    :param str q: - поисковый запрос тайтла. По умолчанию experiments lain.
+    в этом скрипте, то переопределите название.
     """
-    print(f" Run {anime.__module__}")
-    episodes = anime.search(q)[0].episodes()
+    print(f" {anime.__module__}")
+    query, episodes_count = anime._TESTS.get("search")
+    episodes = anime.search(query)[0].episodes()
     # if episodes return first
-    if len(episodes) == 13:
-        assert len(episodes) == 13
-    # else return dubs first
-    else:
-        assert len(episodes) == 1
+    assert len(episodes) == episodes_count
 
 
-@pytest.mark.parametrize("anime", list(ANIME_HTTP_ALL))
+@pytest.mark.parametrize("anime", ANIME_HTTP_ALL)
 def test_get_ongoings(anime: BaseAnimeHTTP):
     """Тест, поиска онгоингов
 
     Для прохождения теста должен найти более 1 онгоингов"""
-    print(f" Run {anime.__module__}")
-    rez = anime.ongoing()
-    assert len(rez) > 1
+    print(f" {anime.__module__}")
+    if anime._TESTS.get("ongoing"):
+        assert len(anime.ongoing()) > 1
+    else:
+        pytest.skip(f"Ongoings not allowed in {anime.__module__}")
 
 
-@pytest.mark.parametrize("anime,q", [(p, "experiments lain") for p in ANIME_HTTP_ALL])
-def test_get_video(anime: BaseAnimeHTTP, q: str):
+@pytest.mark.parametrize("anime", ANIME_HTTP_ALL)
+def test_get_video(anime: BaseAnimeHTTP):
     """Тест, получения прямой ссылки на видео.
 
     По умолчанию ищет по запросу 'experiments lain' (Эксперименты Лэйн), берёт первый эпизод и первую ссылку на
     озвучку от (XL Media).
     Этот тайтл должен быть гарантированно на сайте. Если это не так и этот парсер должен быть обязательно
     в этом скрипте, то переопределите этот тест.
-
-    :param str q: - поисковый запрос тайтла. По умолчанию experiments lain"""
-    print(f" Run {anime.__module__}")
-    rez = anime.search(q)
+    """
+    print(f" {anime.__module__}")
+    query, episodes_count = anime._TESTS.get("search")
+    rez = anime.search(query)
     url = rez[0].episodes()[0].player()[0].get_video()
-    assert ".m3u8" in url or ".mp4" in url or "sibnet" in url
+    assert is_video(url)
 
 
-@pytest.mark.parametrize("anime,title_name", [(p, "_thisTitleIsNotExist123456") for p in ANIME_HTTP_ALL])
-def test_result_not_found(anime: BaseAnimeHTTP, title_name: str):
-    """Тест на гарантированно не найденный тайтл"""
-    print(f" Run {anime.__module__}")
-    rez = anime.search(title_name)
-    assert len(rez) == 0
+@pytest.mark.parametrize("anime", ANIME_HTTP_ALL)
+def test_result_not_found(anime: BaseAnimeHTTP):
+    """Тест на гарантированно **не найденный** тайтл"""
+    print(f" {anime.__module__}")
+    query = anime._TESTS.get("search_not_found")
+    assert len(anime.search(query)) == 0
 
 
 @pytest.mark.parametrize("anime", ANIME_HTTP_ALL)
 def test_get_ongoing_player(anime: BaseAnimeHTTP):
     """Тест на получение объекта эпизода с онгоинга.
     По умолчанию берёт первый и пробует получить ссылку на видео."""
-    print(f" Run {anime.__module__}")
-    url = anime.ongoing()[0].episodes()[0].player()[0].get_video()
-    assert ".m3u8" in url or ".mp4" in url or "sibnet" in url
+    print(f" {anime.__module__}")
+    if anime._TESTS.get("ongoing"):
+        for i in range(3):
+            ongoing = anime.ongoing()[i]
+            episodes = ongoing.episodes()
+            if len(episodes) > 0:
+                url = episodes[0].player()[0].get_video()
+                assert is_video(url)
+                return
+            if not anime._TESTS.get("search_blocked"):
+                pytest.fail(f"Cannot get episodes from {ongoing}. Maybe it block in your country?")
+    else:
+        pytest.skip(f"Ongoings not allowed in {anime.__module__}")
