862c040981ed0dc21662c85c53fa4da2866f0c9e
Georgiy <{ID}+{username}@users.noreply.github.com>
2022-10-25 16:21:45 +0300

    fix Aniboom link extractor, move re-expressions from defaults to methods

--- anicli_api/decoders.py
+++ anicli_api/decoders.py
@@ -8,21 +8,16 @@ from typing import Optional, Dict, List
 from urllib.parse import urlparse
 import json
 
-from _http import BaseHTTPSync
+from anicli_api._http import BaseHTTPSync
+
+
+# TODO docstrings, base abc class for direct links extractor, doc errors
 
 
 class Kodik(BaseHTTPSync):
     HEADERS = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) "
                              "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.114 Mobile Safari/537.36"}
     REFERER = "https://kodik.info"
-    RE_ERROR = re.compile(r"<title>Error</title>")
-    RE_PATTERNS = (
-        re.compile(r'var type = "(?P<type>.*?)";'),
-        re.compile(r"videoInfo\.hash = '(?P<hash>\w+)';"),
-        re.compile(r'var videoId = "(?P<id>\d+)"'),
-    )
-    RE_PARAMS = re.compile(r"var urlParams = (?P<params>'{.*?}')")
-    RE_VALIDATE_URL = re.compile(r"https://\w+\.\w{2,6}/seria/\d+/\w+/\d{3,4}p")
     BASE_PAYLOAD = {"bad_user": True,
                     "info": "{}"}
     QUALITY = (720, 480, 360)
@@ -35,7 +30,7 @@ class Kodik(BaseHTTPSync):
     def parse(cls, url: str, *, referer: Optional[str] = None, raw_response: Optional[str] = None):
         url = url.split("?")[0]
         if url != cls():
-            raise AttributeError()
+            raise AttributeError
         if not referer:
             referer = cls.REFERER
         if not raw_response:
@@ -59,25 +54,28 @@ class Kodik(BaseHTTPSync):
     @classmethod
     def _parse_payload(cls, response: str) -> Dict:
         payload = cls.BASE_PAYLOAD.copy()
-        if not (result := cls.RE_PARAMS.search(response)):
+        if not (result := re.search(r"var urlParams = (?P<params>'{.*?}')", response)):
             raise TypeError
         result = json.loads(result.groupdict()["params"].strip("'"))
         payload.update(result)  # type: ignore
-        for pattern in cls.RE_PATTERNS:
-            if result := pattern.search(response):
+        for pattern in (r'var type = "(?P<type>.*?)";',
+                        r"videoInfo\.hash = '(?P<hash>\w+)';",
+                        r'var videoId = "(?P<id>\d+)"'
+                        ):
+            if result := re.search(pattern, response):
                 payload.update(result.groupdict())
             else:
-                raise ValueError(f"fail {pattern.pattern}")
+                raise ValueError
         return payload
 
     @classmethod
     def is_kodik(cls, url: str) -> bool:
         """return True if url player is kodik."""
-        return bool(cls.RE_VALIDATE_URL.match(url))
+        return bool(re.match(r"https://\w+\.\w{2,6}/seria/\d+/\w+/\d{3,4}p", url))
 
     @classmethod
     def is_banned(cls, response: str):
-        return bool(cls.RE_ERROR.match(response))
+        return bool(re.match(r"<title>Error</title>", response))
 
     @classmethod
     def _get_api_url(cls, url: str) -> str:
@@ -108,28 +106,31 @@ class Kodik(BaseHTTPSync):
 
 
 class Aniboom(BaseHTTPSync):
-    RE_M3U8 = re.compile(r'"hls":"{\\"src\\":\\"(?P<m3u8>.*\.m3u8)\\"')
-    RE_MPD = re.compile(r'"{\\"src\\":\\"(?P<mpd>.*\.mpd)\\"')
-    RE_M3U8_DATA = re.compile(r'#EXT-X-STREAM-INF:BANDWIDTH=\d+,RESOLUTION=(?P<resolution>\d+x\d+),'
-                              r'CODECS=".*?",AUDIO=".*?" {4}(?P<src>.*?\.m3u8)')
-    REFERER = "https://aniboom.one"
+    REFERER = "https://animego.org/"
     USERAGENT = "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, " \
-                "like Gecko) Chrome/94.0.4606.114 Mobile Safari/537.36 "
+                "like Gecko) Chrome/94.0.4606.114 Mobile Safari/537.36"
     ACCEPT_LANG = "ru-RU"
 
-    def __init__(self):
+    def __init__(self, *, referer: Optional[str] = None):
         super().__init__()
-        self.session.headers.update({"user-agent": self.USERAGENT})
+        if not referer:
+            referer = self.REFERER
+        self.session.headers.update({"user-agent": self.USERAGENT,
+                                     "referer": referer,
+                                     "accept-language": self.ACCEPT_LANG,
+                                     })
+        self.session.headers.pop("x-requested-with")
 
     @classmethod
-    def parse(cls, url: str, referer: Optional[str] = None) -> Dict:
+    def parse(cls, url: str) -> Dict:
         if url != cls():
             raise TypeError
-        if not referer:
-            referer = cls.REFERER
-        response = cls()._aniboom_request(url, referer)
-        links = cls._extract_links(response)
-        links["m3u8"] = cls._parse_m3u8(links["m3u8"], referer)
+        cls_ = cls()
+        response = cls_._aniboom_request(url)
+        links = cls_._extract_links(response)
+        if len(links.keys()) == 0:
+            raise TypeError("links not found")
+        links["m3u8"] = cls_._parse_m3u8(links["m3u8"])
         return links
 
     @staticmethod
@@ -140,31 +141,43 @@ class Aniboom(BaseHTTPSync):
     def __eq__(self, url: str) -> bool:  # type: ignore
         return self.is_aniboom(url)
 
-    def _aniboom_request(self, player_url: str, referer: str) -> str:
+    def _aniboom_request(self, player_url: str) -> str:
         # need set lowercase keys
-        r = self.session.get(player_url, headers={"referer": referer})
+        r = self.session.get(player_url)
         return unescape(r.text)
 
     @classmethod
-    def _parse_m3u8(cls, m3u8_url: str, referer: str) -> Dict:
-        response = cls().session.get(m3u8_url, headers={"Referer": referer,
+    def _parse_m3u8(cls, m3u8_url: str) -> Dict:
+        response = cls().session.get(m3u8_url, headers={"referer": "https://aniboom.one/",
+                                                        "origin": "https://aniboom.one",
                                                         "Accept-Language": cls.ACCEPT_LANG}).text
         result = {}
-        for url_data in cls.RE_M3U8_DATA.finditer(response):
+        base_m3u8_url = m3u8_url.replace("/master.m3u8", "")
+        for url_data in re.finditer(r'#EXT-X-STREAM-INF:BANDWIDTH=\d+,RESOLUTION=(?P<resolution>\d+x\d+),'
+                                    r'CODECS=".*?",AUDIO="\w+"\s(?P<src>\w+\.m3u8)', response):
             if m3u8_dict := url_data.groupdict():
-                result[m3u8_dict["resolution"]] = url_data["src"]
+                result[m3u8_dict["resolution"].split("x")[-1]] = f"{base_m3u8_url}{url_data['src']}"
         return result
 
     @classmethod
     def _extract_links(cls, raw_response: str) -> Dict:
         raw_response = unescape(raw_response)
         result = {}
-        if m3u8_url := cls.RE_M3U8.search(raw_response):
+        if m3u8_url := re.search(r'"hls":"{\\"src\\":\\"(?P<m3u8>.*\.m3u8)\\"', raw_response):
             result.update(m3u8_url.groupdict())
-        if mpd_url := cls.RE_MPD.search(raw_response):
+        else:
+            result["m3u8"] = None
+
+        if mpd_url := re.search(r'"{\\"src\\":\\"(?P<mpd>.*\.mpd)\\"', raw_response):
             result.update(mpd_url.groupdict())
+        else:
+            result["mpd"] = None
+
+        for k, v in result.items():
+            result[k] = v.replace("\\", "")
         return result
 
 
 if __name__ == '__main__':
-    Kodik.parse("https://kodik.info/seria/1026046/02a256101df196484d68d10d28987fbb/720p")
+    # Kodik.parse("https://kodik.info/seria/1026046/02a256101df196484d68d10d28987fbb/720p")
+    print(Aniboom.parse('https://aniboom.one/embed/N9QdKm4Mwz1?episode=1&translation=2'))
