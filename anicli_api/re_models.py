623fe67dfd9551f350d9a0e5161efccedf000d3a
Georgiy <{ID}+{username}@users.noreply.github.com>
2022-10-25 16:22:52 +0300

    rename functions params, refactoring before_exec_type, after_exec_type typehints

--- anicli_api/re_models.py
+++ anicli_api/re_models.py
@@ -1,7 +1,12 @@
 from __future__ import annotations
 
 import re
-from typing import Pattern, Optional, Any, Type, Union, AnyStr, Dict, Iterable, Callable
+from typing import Pattern, Optional, Any, Type, Union, AnyStr, Dict, Iterable, Callable, List, Generator
+
+T_BEFORE_EXEC = Union[Callable[[str], str], Dict[str, Callable[[str], str]]]
+T_AFTER_EXEC = Union[Callable[[Any], Any], Dict[str, Callable[[Any], Any]]]
+
+# TODO write docstring, add tests
 
 
 class ReBaseField:
@@ -24,21 +29,22 @@ class ReBaseField:
 
         3. return {name: result}
     """
+
     def __init__(self,
                  pattern: Union[Pattern, AnyStr],
                  *,
                  name: Optional[str] = None,
                  default: Optional[Any] = None,
-                 result_type: Type = str,
-                 before_func: Optional[Union[Callable, Dict[str, Callable]]] = None,
-                 after_func: Optional[Union[Callable, Dict[str, Callable]]] = None):
+                 type: Type = str,
+                 before_exec_type: Optional[T_BEFORE_EXEC] = None,
+                 after_exec_type: Optional[T_AFTER_EXEC] = None):
         self.pattern = pattern if isinstance(pattern, Pattern) else re.compile(pattern)
 
         self.name = self._set_name(name)
 
-        self.result_type = result_type
-        self.before_func = before_func
-        self.after_func = after_func
+        self.result_type = type
+        self.before_func = before_exec_type
+        self.after_func = after_exec_type
         self.default = default
         self.value = None
 
@@ -51,7 +57,7 @@ class ReBaseField:
     @staticmethod
     def _init_lambda_function(*,
                               value: Any,
-                              func: Optional[Union[Dict[str, Callable], Callable]] = None,
+                              func: Optional[Union[Dict[str, Callable[[Any], Any]], Callable[[Any], Any]]] = None,
                               key: str = "",
                               ) -> Any:
         if func:
@@ -65,21 +71,28 @@ class ReBaseField:
 
     def _enchant_value(self, key: str, value: Any):
         value = self._init_lambda_function(func=self.before_func, key=key, value=value)
-        if val := self.before_func_call(key=key, value=value):
+        if val := self.before_exec_func_call(key=key, value=value):
             value = val
         value = self._set_type(value)
-        if val := self.after_func_call(key=key, value=value):
+        if val := self.after_exec_func_call(key=key, value=value):
             value = val
         value = self._init_lambda_function(func=self.after_func, key=key, value=value)
         return value
 
-    def parse(self, text: str):
+    def parse(self, text: str) -> Dict[str, Any]:
         raise NotImplementedError
 
-    def before_func_call(self, key: str, value: Any):
+    def parse_values(self, text: str) -> List[Any]:
+        rez = []
+        for val in self.parse(text).values():
+            rez.extend(val) if isinstance(val, List) else rez.append(val)
+
+        return rez
+
+    def before_exec_func_call(self, key: str, value: Any):
         ...
 
-    def after_func_call(self, key: str, value: Any):
+    def after_exec_func_call(self, key: str, value: Any):
         ...
 
     def _set_type(self, value: Any) -> Type[Any]:
@@ -115,9 +128,9 @@ class ReFieldList(ReBaseField):
                  *,
                  name: str,
                  default: Optional[Iterable[Any]] = None,
-                 result_type: Type = str,
-                 before_func: Optional[Callable] = None,
-                 after_func: Optional[Callable] = None):
+                 type: Type = str,
+                 before_exec_type: Optional[Callable] = None,
+                 after_exec_type: Optional[Callable] = None):
 
         if not default:
             default = []
@@ -129,11 +142,11 @@ class ReFieldList(ReBaseField):
             default = list(default)
 
         super().__init__(pattern, name=name, default=default,
-                         result_type=result_type, before_func=before_func, after_func=after_func)
+                         type=type, before_exec_type=before_exec_type, after_exec_type=after_exec_type)
 
-    def parse(self, text: str) -> Dict:
+    def parse(self, text: str) -> Dict[str, List]:
         if not (result := self.pattern.findall(text)):
-            return {self.name: self.default}
+            return {self.name: self.default}  # type: ignore
         for i, el in enumerate(result):
             el = self._enchant_value("", el)
             result[i] = el
@@ -146,9 +159,9 @@ class ReFieldListDict(ReBaseField):
                  *,
                  name: str,
                  default: Optional[Iterable[Any]] = None,
-                 result_type: Type = str,
-                 before_func: Optional[Dict[str, Callable]] = None,
-                 after_func: Optional[Dict[str, Callable]] = None):
+                 type: Type = str,
+                 before_exec_type: Optional[Dict[str, Callable]] = None,
+                 after_exec_type: Optional[Dict[str, Callable]] = None):
         if not default:
             default = []
 
@@ -156,11 +169,11 @@ class ReFieldListDict(ReBaseField):
             raise TypeError
 
         super().__init__(pattern, name=name, default=default,
-                         result_type=result_type, before_func=before_func, after_func=after_func)
+                         type=type, before_exec_type=before_exec_type, after_exec_type=after_exec_type)
 
-    def parse(self, text: str) -> Dict:
+    def parse(self, text: str) -> Dict[str, List[Any]]:
         if not self.pattern.search(text):
-            return {self.name: self.default}
+            return {self.name: self.default}  # type: ignore
         if not self.pattern.groupindex:
             raise TypeError
         values = []
@@ -172,7 +185,7 @@ class ReFieldListDict(ReBaseField):
         return {self.name: values}
 
 
-def parse_many(text: str, *re_fields: ReBaseField) -> dict:
+def parse_many(text: str, *re_fields: ReBaseField) -> dict[str, Any]:
     result = {}
     for re_field in re_fields:
         result.update(re_field.parse(text))
@@ -181,17 +194,31 @@ def parse_many(text: str, *re_fields: ReBaseField) -> dict:
 
 if __name__ == '__main__':
     tst = "foo=123, bar=based 9129 800 1 92"
-    f = ReField(r"foo=(?P<digit>\d+)", result_type=int)
-    f2 = ReField(r"foo=(\d+)", result_type=float, name="float_digit")
+    f = ReField(r"foo=(?P<digit>\d+)",  # set key "digit"
+                type=int)  # convert result to int
+    f2 = ReField(r"foo=(\d+)",  # without pattern.groupindex
+                 type=float,  # convert to float
+                 name="float_digit")  # set key "float_digit"
 
-    f3 = ReFieldList(r"(\d+)", result_type=int, name="digit_list",
-                     before_func=lambda s: f"{s}0",
-                     after_func=lambda s: s * 5)
+    f3 = ReFieldList(r"(\d+)",
+                     type=int,  # convert all result to int
+                     name="lst_digit",  # name
+                     before_exec_type=lambda s: f"{s}0",  # add "0" to match
+                     after_exec_type=lambda s: s * 5)  # mul integer result to 5
 
-    f4 = ReFieldListDict(r"(?P<key>\w+)=(?P<value>[\d\w]+)", name="lst_dict",
-                         after_func={"value": lambda s: int(s) if s.isdigit() else s.title(),
-                                      "key": lambda s: s.upper()})
+    f4 = ReFieldListDict(r"(?P<key>\w+)=(?P<value>[\d\w]+)",
+                         name="lst_dict",
+                         # use lambda functions to result
+                         after_exec_type={"value": lambda s: int(s) if s.isdigit() else s.title(),
+                                          "key": lambda s: s.upper()})
 
-    f5 = ReField(r"patternnotexist", name="empty")
-
+    f5 = ReField(r"patternnotexist", name="empty")  # default return None
+    print(f3.parse_values(tst))
+    print(f4.parse_values(tst))
+    print(f.parse_values(tst))
     print(parse_many(tst, f, f2, f3, f4, f5))
+    # {'digit': 123,
+    # 'float_digit': 123.0,
+    # 'lst_digit': [6150, 456450, 40000, 50, 4600],
+    # 'lst_dict': [{'key': 'FOO', 'value': 123}, {'key': 'BAR', 'value': 'Based'}],
+    # 'empty': None}
