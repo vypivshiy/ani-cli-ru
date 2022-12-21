e02ec84fba1900bb56b5b8631d7031489328ed75
georgiy <59173419+vypivshiy@users.noreply.github.com>
2022-12-21 21:48:26 +0300

    add word completer

--- anicli/commands.py
+++ anicli/commands.py
@@ -5,7 +5,7 @@ from prompt_toolkit import PromptSession
 from anicli_api.base import MetaVideo
 from anicli_api.extractors import animego
 from anicli.config import app
-from anicli.utils import number_validator
+from anicli.utils import number_validator, make_completer
 
 EXTRACTOR = animego.Extractor()
 PLAYER = "mpv"
@@ -24,40 +24,42 @@ def mpv_attrs(video: MetaVideo) -> list[str]:
 def get_video(session: PromptSession, anime_info: animego.AnimeInfo):
     episodes = anime_info.get_episodes()
     print(*[f"[{i}] {e}" for i, e in enumerate(episodes)], sep="\n")
-    num = int(session.prompt("[EPISODE] > ", validator=number_validator(len(episodes))))
+    num = int(session.prompt("[EPISODE] > ",
+                             validator=number_validator(episodes),
+                             completer=make_completer(episodes)))
 
     videos = episodes[num].get_videos()
     print("choose video host:")
     print(*[f"{i} {v}" for i, v in enumerate(videos)], sep="\n")
-    num = int(session.prompt("[VIDEO] > ", validator=number_validator(len(videos))))
+    num = int(session.prompt("[VIDEO] > ",
+                             validator=number_validator(videos),
+                             completer=make_completer(videos)))
 
     sources = videos[num].get_source()
     print("Quality:")
     print(*[f"{i} {v.type} {v.quality}" for i, v in enumerate(sources)], sep="\n")
-    num = int(session.prompt("[QUALITY] > ", validator=number_validator(len(sources))))
+    num = int(session.prompt("[QUALITY] > ",
+                             validator=number_validator(sources),
+                             completer=make_completer(sources)))
 
     args = mpv_attrs(sources[num])
     subprocess.run(" ".join(args), shell=True)
 
 
-def _is_not_empty_query(*args: str):
-    if not args:
-        print("input query")
-        return False
-    return True
-
-
 @app.command(["search", "find"], "search anime titles by query",
-             args_hook=lambda *args: (" ".join(list(args)),),
-             rule=_is_not_empty_query)
+             args_hook=lambda *args: (" ".join(list(args)),))
 def search(query: str):
     results = EXTRACTOR.search(query)
     if len(results) > 0:
         print(*[f"{i} {r}" for i, r in enumerate(results)], sep="\n")
         print("choose title")
         session = app.new_prompt_session()
-        num = int(session.prompt("[SEARCH] > ", validator=number_validator(len(results))))
+        num = int(session.prompt("[SEARCH] > ",
+                                 validator=number_validator(results),
+                                 completer=make_completer(results)
+                                 ))
         anime_info = results[num].get_anime()
+        print(anime_info)
         get_video(session, anime_info)
     else:
         print("Not found")
@@ -71,7 +73,10 @@ def ongoing():
         print(*[f"{i} {r}" for i, r in enumerate(results)], sep="\n")
         print("choose title")
         session = app.new_prompt_session()
-        num = int(session.prompt("[ONGOING] > ", validator=number_validator(len(results))))
+        num = int(session.prompt("[ONGOING] > ",
+                                 validator=number_validator(results),
+                                 completer=make_completer(results)
+                                 ))
         anime_info = results[num].get_anime()
         get_video(session, anime_info)
     else:
--- anicli/utils.py
+++ anicli/utils.py
@@ -1,6 +1,7 @@
 from functools import partial
-from typing import Callable
+from typing import Callable, Union, Any
 
+from prompt_toolkit.completion import WordCompleter
 from prompt_toolkit.validation import Validator
 
 
@@ -9,10 +10,18 @@ def make_validator(function: Callable[..., bool], *args, **keywords) -> Validato
     return Validator.from_callable(function)
 
 
+def make_completer(results: list[Any]) -> WordCompleter:
+    return WordCompleter(words=[str(i) for i in range(len(results))],
+                         meta_dict={str(i): str(el) for i, el in enumerate(results)})
+
+
 def _number_func(digit: str, max_len: int):
     return digit.isdigit() and 0 <= int(digit) < max_len
 
 
-def number_validator(max_len: int) -> Validator:
+def number_validator(max_len: Union[int, list]) -> Validator:
+    if isinstance(max_len, list):
+        max_len = len(max_len)
     func = partial(_number_func, max_len=max_len)
     return Validator.from_callable(func, error_message=f"Should be integer and (0<=n<{max_len})")
+
