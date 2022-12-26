323972218b50c84f9ce4e48930078259761324d2
Georgiy <{ID}+{username}@users.noreply.github.com>
2022-12-26 14:50:48 +0300

    refactoring storages, add params storage and cache objects

--- anicli/core/states.py
+++ anicli/core/states.py
@@ -1,53 +1,104 @@
 from __future__ import annotations
 from enum import Enum
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Tuple, Hashable, Sequence
+
+
+class BaseStorage:
+    def __init__(self):
+        self._data: Dict[Hashable, Any] = {}
+
+    def __iter__(self):
+        return self._data.__iter__()
+
+    def __contains__(self, item):
+        return item in self._data
+
+    def update(self, data: Dict):
+        self._data.update(data)
+
+    def clear(self):
+        self._data.clear()
+
+    def get(self, key):
+        return self._data.get(key, None)
+
+    def __getitem__(self, item):
+        return self._data[item]
+
+    def __setitem__(self, key, value):
+        self._data[key] = value
+
+    def __str__(self):
+        return f"Data: {self._data}"
+
+
+class Storage(BaseStorage):
+    def __init__(self):
+        super().__init__()
+        self._data: Dict[BaseState, Dict[str, Any]] = {}
+
+
+class StorageParams(BaseStorage):
+    def __init__(self):
+        super().__init__()
+        self._data: Dict[BaseState, Tuple]
 
 
 class StateDispenser:
     def __init__(self):
         self.state: Optional[BaseState] = None
-        self._stored_params: Dict[BaseState, Dict[str, Any]] = {}
-        self.data = {}
-
-    def store_params(self, state: BaseState, **params):
-        self._stored_params.update({state: params})
-
-    def get_stored_params(self, state: BaseState) -> tuple[Any, ...]:
-        if params := self._stored_params.get(state):
-            return tuple(params.values())
-        return ()
+        self.storage = Storage()
+        self.storage_params = StorageParams()
 
     def set(self, state: BaseState):
         self.state = state
 
     def finish(self):
-        self.clear()
-        self._stored_params.clear()
+        self.storage.clear()
+        self.storage_params.clear()
         self.state = None
 
-    def clear(self):
-        self.data.clear()
+    @staticmethod
+    def _create_hash_key(obj: Any) -> int:
+        if isinstance(obj, Hashable):
+            key = hash(obj)
+        elif isinstance(obj, dict):
+            key = hash(obj.keys())
+        elif isinstance(obj, Sequence):
+            key = hash(tuple(obj))
+        else:
+            raise TypeError(f"`{obj}` failed create hash key")
+        return key
+
+    def cache_object(self, key: Any, obj: Any):
+        key = self._create_hash_key(key)
+        if self.storage.get(key):
+            return True
+        self.storage[key] = obj
+        return False
+
+    def get_cache(self, key: Any):
+        key = self._create_hash_key(key)
+        return self.storage.get(key)
+
+
+    def update(self, data: Dict):
+        self.storage.update(data)
+
+    def get(self, key):
+        return self.storage.get(key)
+
+    def __getitem__(self, item):
+        return self.storage[item]
+
+    def __setitem__(self, key, value):
+        self.storage[key] = value
 
     def __bool__(self):
         return bool(self.state)
 
-    def __iter__(self):
-        return self.data.__iter__()
-
-    def __contains__(self, item):
-        return item in self.data
-
-    def update_data(self, data: Dict[str, Any]):
-        self.data.update(data)
-
-    def __getitem__(self, item):
-        return self.data[item]
-
-    def __setitem__(self, key, value):
-        self.data[key] = value
-
     def __str__(self):
-        return f"State: {self.state} storage data: {self.data}"
+        return f"Storage: {self.storage} Params: {self.storage_params}"
 
 
 class BaseState(str, Enum):
