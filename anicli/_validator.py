605e9f601b4d5e06cca773af1704f573712bb682
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-07-18 21:47:44 +0300

    fix slice validator

--- anicli/_validator.py
+++ anicli/_validator.py
@@ -18,24 +18,6 @@ class NumPromptValidator(Validator):
         raise ValidationError(message="Should be digit, or `..` `~`")
 
 
-class SliceNumPromptValidator(Validator):
-    def __init__(self, items_list: List[Any]):
-        self.items_list = items_list
-        self.items_len = len(items_list)
-
-    def validate(self, document: Document) -> None:
-        text = document.text
-        if text.isdigit() and -1 < int(text) < self.items_len:
-            return
-        elif len(text.split("-")) == 2:
-            start, end = text.split("-")
-            if start.isdigit() and end.isdigit() and -1 < int(start) < int(end) >= self.items_len:
-                return
-        elif text in ("..", "~"):
-            return
-        raise ValidationError(message="Should be digit, or `..` `~` or slice eg 0-2")
-
-
 class AnimePromptValidator(Validator):
     def __init__(self, items_list: List[Any]):
         self.items_list = items_list
@@ -49,6 +31,11 @@ class AnimePromptValidator(Validator):
             return
         elif len(text.split("-")) == 2:
             start, end = text.split("-")
-            if start.isdigit() and end.isdigit() and -1 < int(start) < int(end) >= self.items_len:
+            if start.isdigit() and end.isdigit() and -1 < int(start) < int(end) <= self.items_len:
+                if int(start) >= int(end):
+                    raise ValidationError(message="Wrong slice range")
                 return
+            else:
+                raise ValidationError(message="Wrong slice range")
+
         raise ValidationError(message=f"Should be digit, slice `0-{self.items_len}` or (`..`, `~`, `info`)")
