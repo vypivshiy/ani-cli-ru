8955ae4104a44d1502b98d3ad6f20298481e3f43
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-07-18 21:47:34 +0300

    add documentation, add auto suggest

--- anicli/cli/config.py
+++ anicli/cli/config.py
@@ -7,9 +7,7 @@ from eggella import Eggella
 from anicli_api.source import animego
 
 from prompt_toolkit.styles.pygments import style_from_pygments_cls
-from pygments.styles import get_style_by_name
-
-style = style_from_pygments_cls(get_style_by_name('monokai'))
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
 logger = logging.getLogger("scrape_schema")
 logger.setLevel(logging.DEBUG)
@@ -18,9 +16,12 @@ logger.addHandler(
     logging.FileHandler("logs.log")
 )
 
-app = Eggella("anicli")
-app.session = PromptSession("~ ", history=FileHistory(".anicli_history"), style=style)
+app = Eggella("anicli", "~ ")
+app.session = PromptSession("~ ", history=FileHistory(".anicli_history"), auto_suggest=AutoSuggestFromHistory())
+app.documentation = """Anicli
 
+This is a TUI client for watching anime.
+"""
 EXTRACTOR = animego.Extractor()
 PLAYER = "mpv"
 # TODO add vlc and implement simple proxy server for redirects
