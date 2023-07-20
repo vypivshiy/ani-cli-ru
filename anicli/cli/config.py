f0a0e8f7c130f778656179ff27f534d8cb04667f
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-07-20 12:39:57 +0300

    add CFG class storage configuration

--- anicli/cli/config.py
+++ anicli/cli/config.py
@@ -1,34 +1,33 @@
-import logging
+from typing import TYPE_CHECKING
 
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 from eggella import Eggella
 
-from anicli_api.source import animego
 
-from prompt_toolkit.styles.pygments import style_from_pygments_cls
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
-# logger = logging.getLogger("scrape_schema")
-# logger.setLevel(logging.DEBUG)
-# print(logger.handlers.pop(0))
-# logger.addHandler(
-#     logging.FileHandler("logs.log")
-# )
+if TYPE_CHECKING:
+    from anicli_api.base import BaseExtractor
 
-app = Eggella("anicli", "~ ")
+
+class Config:
+    EXTRACTOR: "BaseExtractor" = NotImplemented
+    PLAYER: str = "mpv"
+    PLAYER_EXTRA_ARGS: str = ""
+    MIN_QUALITY: int = 0
+    USE_FFMPEG_ROUTE: bool = False
+    _CONFIG_PATH: str = "~/.config/ruanicli"
+
+
+class AnicliApp(Eggella):
+    CFG = Config()
+
+
+app = AnicliApp("anicli", "~ ")
 app.session = PromptSession("~ ", history=FileHistory(".anicli_history"), auto_suggest=AutoSuggestFromHistory())
+
 app.documentation = """Anicli
 
-This is a TUI client for watching anime.
+This is a simple a TUI client for watching anime.
 """
-PLAYER = "mpv"
-# TODO add vlc and implement simple proxy server for redirects
-
-PLAYER_ARGS = ""
-
-# config path
-# unix
-# PATH = "~/.config/ruanicli"
-# windows
-# PATH = "%Appdata%/local/ruanicli"
