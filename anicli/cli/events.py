daf0f1109d71af32ba6f066a61fcb13f790ea786
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-07-20 17:02:42 +0300

    add setup http class config, disable scrape_schema logging

--- anicli/cli/events.py
+++ anicli/cli/events.py
@@ -1,6 +1,21 @@
+import logging
+
 from anicli.cli.config import app
 from urllib.parse import urlsplit
+from anicli_api._http import HTTPSync
 
+
+@app.on_startup()
+def setup_http_config():
+    if app.CFG.PROXY:
+        app.cmd.print_ft("Setup proxy")
+    if app.CFG.TIMEOUT:
+        app.cmd.print_ft("Setup timeout")
+    HTTPSync(**app.CFG.httpx_kwargs())
+
+@app.on_startup()
+def sc_schema_set_logging():
+    logging.getLogger("scrape_schema").setLevel(logging.ERROR)
 
 @app.on_startup()
 def loaded_extractor_msg():
