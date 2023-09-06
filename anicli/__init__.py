3a5ed698779ae39fec005aa5ec03c3713f872dcd
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-09-06 10:03:31 +0300

    implemented app config

--- anicli/__init__.py
+++ anicli/__init__.py
@@ -1,54 +1,88 @@
-from anicli.cli import APP
+import argparse
 import importlib
+from typing import Any, Dict
+
+from anicli.cli import APP
+from anicli.cli.config import get_file_config
+from anicli.cli.compat import tomllib
+from anicli.cli_utlis import is_ffmpeg_installed, is_player_installed
+
 __version__ = "5.0.3"
+
+
+def _read_file_config() -> Dict[str, Any]:
+    with open(get_file_config(), "r") as f:
+        cfg = tomllib.load(f)
+    return cfg
+
+
+def _run_app(namespaces: argparse.Namespace):
+    """parse config and run application"""
+    cfg = _read_file_config()
+    module_name = namespaces.source or cfg['source']
+    module = importlib.import_module(f"anicli_api.source.{module_name}")
+    APP.CFG.EXTRACTOR = getattr(module, "Extractor")()
+    APP.CFG.USE_FFMPEG_ROUTE = namespaces.ffmpeg or cfg["ffmpeg_proxy"]
+    APP.CFG.PLAYER = namespaces.player or cfg["player"]
+    APP.CFG.PLAYER_ARGS = cfg["player_arguments"]
+
+    if not is_player_installed(APP.CFG.PLAYER):
+        print(f"Error! Player {APP.CFG.PLAYER} not found")
+        exit(1)
+
+    APP.CFG.MIN_QUALITY = namespaces.quality or cfg['minimal_quality']
+    APP.CFG.TIMEOUT = namespaces.timeout or cfg['timeout']
+    APP.CFG.PROXY = namespaces.proxy or cfg["proxy"]
+    APP.loop()
 
 
 def run_cli():
     import argparse
     parser = argparse.ArgumentParser(description="anicli-ru")
-    parser.add_argument("-s", "--source", default="animego", choices=["animego", "sovetromantica",
+    parser.add_argument("-s", "--source", choices=["animego", "sovetromantica",
                                                                       "animejoy",
                                                                       "anilibria",
                                                                       "animevost"],
                         help="Anime source provider. DEFAULT `animego`")
     parser.add_argument("-q", "--quality",
                         type=int,
-                        default=0,
                         choices=[0, 144, 240, 360, 480, 720, 1080],
                         help="Set minimal video quality output. "
                              "If there is no maximum, it will display the closest value"
                         )
     parser.add_argument("-p", "--player",
                         type=str,
-                        default="mpv",
                         choices=["mpv", "vlc", "cvlc"],
                         help="Set videoplayer target. Default mpv"
                         )
 
     parser.add_argument("--ffmpeg",
                         action="store_true",
-                        default=False,
-                        help="usage ffmpeg backend for redirect video to player. "
-                             "Enable, if your player cannot accept headers params"
+                        help="usage ffmpeg proxy for redirect video to player. "
+                             "Enable, if your player cannot accept headers params or stream video"
+                             "from the internet"
                         )
     parser.add_argument("--proxy",
                         type=str,
-                        default=None,
                         help="Setup proxy")
     parser.add_argument("--timeout",
                         type=float,
-                        default=None,
                         help="Setup timeout")
 
+    parser.add_argument("--version",
+                        action="store_true",
+                        default=False,
+                        help="Show app version")
+
     namespaces = parser.parse_args()
-    module = importlib.import_module(f"anicli_api.source.{namespaces.source}")
-    APP.CFG.EXTRACTOR = getattr(module, "Extractor")()
-    APP.CFG.USE_FFMPEG_ROUTE = namespaces.ffmpeg
-    APP.CFG.PLAYER = namespaces.player
-    APP.CFG.MIN_QUALITY = namespaces.quality
-    APP.CFG.TIMEOUT = namespaces.timeout
-    APP.CFG.PROXY = namespaces.proxy
-    APP.loop()
+    if namespaces.version:
+        print("anicli-ru", __version__)
+        exit(0)
+    if namespaces.ffmpeg and not is_ffmpeg_installed():
+        print("ffmpeg not found, please, install ffmpeg")
+        exit(1)
+
+    _run_app(namespaces)
 
 
 if __name__ == '__main__':
--- anicli/cli/config.py
+++ anicli/cli/config.py
@@ -1,38 +1,90 @@
+import sys
 from typing import TYPE_CHECKING, Optional
 from pathlib import Path
 
+from eggella import Eggella
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.shortcuts.prompt import CompleteStyle
-from eggella import Eggella
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+
+from anicli.cli.compat import tomllib
 
 if TYPE_CHECKING:
     from anicli_api.base import BaseExtractor
+
+_DEFAULT_APP_CONFIG = {
+    "source": "animego",
+    "minimal_quality": 0,
+    "player": "mpv",
+    "ffmpeg_proxy": False,
+    "proxy": "",
+    "timeout": "",
+    # ADVANCED player keys config
+    "player_arguments": {
+        "cmd_title": '--title="{}"',
+        "cmd_headers_arg": '--http-header-fields="{}: {}"',
+        "cmd_extra_args": ""
+    }
+}
+
+
+def _is_exists_config():
+    path = get_path_config()
+    file = path / "config.toml"
+    return file.exists()
+
+
+def _create_new_config():
+    """create default file config if not exists()"""
+    if not _is_exists_config():
+        with open(get_path_config() / "config.toml", "w") as f:
+            f.write(tomllib.dumps(_DEFAULT_APP_CONFIG))
+
+
+def get_file_config() -> Path:
+    if not _is_exists_config():
+        _create_new_config()
+    return get_path_config() / "config.toml"
+
+
+def get_path_config() -> Path:
+    if sys.platform == "win32":
+        path = Path.home() / "AppData" / "Roaming" / "anicliru"
+        path.mkdir(exist_ok=True, parents=True)
+        return path
+
+    path = Path.home() / ".config" / "anicliru"
+    path.mkdir(exist_ok=True, parents=True)
+    return path
 
 
 class Config:
     EXTRACTOR: "BaseExtractor" = NotImplemented
     PLAYER: str = "mpv"
-    PLAYER_EXTRA_ARGS: str = ""
     MIN_QUALITY: int = 0
     USE_FFMPEG_ROUTE: bool = False
-    CONFIG_PATH: str = Path.home() / ".config" / "ruanicli"
+    CONFIG_PATH: str = str(get_file_config())
 
     PROXY: Optional[str] = None
     TIMEOUT: Optional[float] = None
+
+    PLAYER_ARGS: dict[str, str] = {}
 
     @classmethod
     def httpx_kwargs(cls):
         return {"proxies": cls.PROXY, "timeout": cls.TIMEOUT}
 
 
+# app configuration
 class AnicliApp(Eggella):
     CFG = Config()
 
 
 app = AnicliApp("anicli", "~ ")
-app.session = PromptSession("~ ", history=FileHistory(".anicli_history"), auto_suggest=AutoSuggestFromHistory(),
+app.session = PromptSession("~ ",
+                            history=FileHistory(str(get_path_config() / ".anicli_history")),
+                            auto_suggest=AutoSuggestFromHistory(),
                             complete_style=CompleteStyle.MULTI_COLUMN)
 
 app.documentation = """Anicli
