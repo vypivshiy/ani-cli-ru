6d0925649e0452abdb89b2e259df19589579e143
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-12-12 18:16:38 +0300

    add get_preferred_quality,  is_video_url_valid functions

--- anicli/cli/video_utils.py
+++ anicli/cli/video_utils.py
@@ -1,6 +1,8 @@
 
 from typing import TYPE_CHECKING, List, Generator, Tuple, cast
 from urllib.parse import urlsplit
+from httpx import Client
+
 
 from anicli.cli.config import AnicliApp
 
@@ -8,8 +10,6 @@ if TYPE_CHECKING:
     from anicli_api.base import BaseSource, BaseEpisode
     from anicli_api.player.base import Video
     from anicli.cli.config import Config
-
-__all__ = ["slice_play_hash", "slice_playlist_iter", "sort_video_by_quality"]
 
 
 def slice_play_hash(video: "Video", source: "BaseSource"):
@@ -44,3 +44,20 @@ def sort_video_by_quality(videos: List["Video"], quality: int) -> List["Video"]:
         return result
     # not founded, get maximum value
     return [max(videos, key=lambda v: v.quality)]
+
+
+def get_preferred_quality_index(videos: List["Video"], quality: int) -> int:
+    max_quality = 0
+    i = 0
+    for i, video in enumerate(videos):
+        if video.quality >= quality:
+            return i
+        max_quality = max(video.quality, max_quality)
+    return i
+
+
+def is_video_url_valid(video: "Video") -> bool:
+    # head method maybe don't work, but we can send GET request without read all content
+    with Client().stream("GET", video.url, headers=video.headers) as r:
+        # 200-299 OK, 302 - FOUND returns True, else False
+        return r.is_success or r.status_code == 302
