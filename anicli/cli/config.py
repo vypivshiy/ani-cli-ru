032909a938f2b83469086d8901c128bab1cf6296
georgiy <59173419+vypivshiy@users.noreply.github.com>
2023-12-12 18:14:35 +0300

    add config patch constants, add exist_config method

--- anicli/cli/config.py
+++ anicli/cli/config.py
@@ -1,10 +1,13 @@
 from typing import TYPE_CHECKING, Optional
 from pathlib import Path
+# import tomli
+
 
 from prompt_toolkit import PromptSession
 from prompt_toolkit.history import FileHistory
 from prompt_toolkit.shortcuts.prompt import CompleteStyle
 from eggella import Eggella
+
 from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
 if TYPE_CHECKING:
@@ -15,10 +18,12 @@ class Config:
     EXTRACTOR: "BaseExtractor" = NotImplemented
     PLAYER: str = "mpv"
     PLAYER_EXTRA_ARGS: str = ""
-    MIN_QUALITY: int = 0
+    MIN_QUALITY: int = 1080
     USE_FFMPEG_ROUTE: bool = False
-    CONFIG_PATH: str = Path.home() / ".config" / "ruanicli"
-
+    _CONFIG_PATH: str = "~/.config/ruanicli"
+    _CONFIG_NAME: str = "config.toml"
+    _DB_NAME: str = "anicli.db"
+    # httpx params
     PROXY: Optional[str] = None
     TIMEOUT: Optional[float] = None
 
@@ -26,13 +31,32 @@ class Config:
     def httpx_kwargs(cls):
         return {"proxies": cls.PROXY, "timeout": cls.TIMEOUT}
 
+    @classmethod
+    def exists_config(cls) -> bool:
+        cfg_path = Path(cls._CONFIG_PATH) / cls._CONFIG_NAME
+        return Path(cls._CONFIG_PATH).exists() and cfg_path.exists()
+
+    # @classmethod
+    # def set_up_config(cls):
+    #     if not cls.exists_config():
+    #         folder = Path(cls._CONFIG_PATH)
+    #         if not folder.exists():
+    #             folder.mkdir()
+    #         cfg_path = folder / cls._CONFIG_NAME
+    #         if not cfg_path.exists():
+    #             cfg_path.touch()
+    #             for k,v in cls.__dict__.items():
+
+
 
 class AnicliApp(Eggella):
     CFG = Config()
 
 
 app = AnicliApp("anicli", "~ ")
-app.session = PromptSession("~ ", history=FileHistory(".anicli_history"), auto_suggest=AutoSuggestFromHistory(),
+app.session = PromptSession("~ ",
+                            history=FileHistory(".anicli_history"),
+                            auto_suggest=AutoSuggestFromHistory(),
                             complete_style=CompleteStyle.MULTI_COLUMN)
 
 app.documentation = """Anicli
