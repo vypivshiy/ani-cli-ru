ed4488cdddfd543c3a251fdbd3146c9f6df69be9
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-08-06 00:52:18 +0300

    add tooltips(demo), color loading indicators to green

---
+++ anicli/tooltips.py
@@ -0,0 +1,8 @@
+APP_SEARCH_INPUT = """
+Type search title query or filter ongoings list. 
+Press [bold]ENTER[/] to start searching or press [bold]Search[/] button
+""".strip()
+
+APP_ONGOING_CONTAINER = """
+Click to item or press [bold]ENTER[/] in selected item
+""".strip()
--- anicli/tui/main.py
+++ anicli/tui/main.py
@@ -7,8 +7,9 @@ from textual import on, work
 from textual.app import App, ComposeResult
 from textual.containers import Horizontal, Vertical
 from textual.reactive import reactive
-from textual.widgets import Footer, Input, Button, ListView, LoadingIndicator
+from textual.widgets import Footer, Input, Button, ListView
 
+from anicli import tooltips as _
 from .components import AppHeader, AnimeListItem
 from .player.mpv import run_video
 from .screens import AnimeResultScreen, SearchResultScreen, SourceResultScreen, VideoResultScreen
@@ -18,6 +19,8 @@ from ..utils.cached_extractor import CachedExtractor
 class AnicliRuTui(App):
     BINDINGS = [
         ("ctrl+d", "toggle_dark", "Toggle dark mode"),
+        ("ctrl+b", "pop_screen", "back to previous screen"),
+        ("ctrl+h", "help_screen", "show help page"),
         # ("ctrl+s", "toggle_sidebar", "Toggle sidebar")
     ]
     CSS_PATH = 'tui.css'
@@ -29,16 +32,23 @@ class AnicliRuTui(App):
         """Event handler called when widget is added to the app."""
         self.update_ongoings()
         self.query_one('#ongoing-container').border_title = 'Ongoings'
+        self.query_one('#ongoing-container').tooltip = _.APP_ONGOING_CONTAINER
+
         self.query_one('#search-container').border_title = 'Search or filter ongoings'
+
+        self.query_one('#search-input').tooltip = _.APP_SEARCH_INPUT
 
     @work(exclusive=True)
     async def update_ongoings(self) -> None:
-        self.ongoings = await self.extractor.a_ongoing()
         ongs_view = self.query_one('#ongoings-items', ListView)
+
+        self.query_one('#search-input').disabled = True
+        ongs_view.loading = True
+        self.ongoings = await self.extractor.a_ongoing()
         await ongs_view.extend([AnimeListItem(i, o) for i, o in enumerate(self.ongoings, 1)])
 
-        loading_ico = self.query_one('#ongoings-loads', LoadingIndicator)
-        loading_ico.styles.display = 'none'
+        self.query_one('#search-input').disabled = False
+        ongs_view.loading = False
 
     def compose(self) -> ComposeResult:
         yield AppHeader(id='header')
@@ -47,7 +57,6 @@ class AnicliRuTui(App):
                 yield Input(placeholder='>', id='search-input')
                 yield Button('Search', id='search-button')
             with Vertical(id='ongoing-container'):
-                yield LoadingIndicator(id='ongoings-loads')
                 yield ListView(id='ongoings-items')
         yield Footer()
 
@@ -66,11 +75,14 @@ class AnicliRuTui(App):
             self.notify('Empty search query', severity='error')
             return
 
+        self.query_one('#search-input').loading = True
         results = await self.extractor.a_search(value)
         if not results:
             self.notify(f'titles by [b]{value}[/b] query not founded', severity='error')
+            self.query_one('#search-input').loading = False
             return
 
+        self.query_one('#search-input').loading = False
         await self.push_screen(SearchResultScreen(search_results=results))
 
     @on(Input.Changed, '#search-input')
@@ -90,29 +102,42 @@ class AnicliRuTui(App):
 
     @on(ListView.Selected, '#ongoings-items, #search-items')
     async def ongoing_or_search_choice(self, event: ListView.Selected):
+        event.list_view.loading = True
         result: SEARCH_OR_ONGOING = event.item.value  # type: ignore
+
         anime = await self.extractor.a_get_anime(result)
         episodes = await self.extractor.a_get_episodes(anime)
         if not episodes:
-            self.notify(f'Episodes not found in [bold]{anime.title}[/]', severity='error')
+            self.notify(f'[bold]{anime.title}[/]: episodes not founded', severity='error')
+            event.list_view.loading = False
             return
+
+        event.list_view.loading = False
         await self.push_screen(AnimeResultScreen(anime, episodes))
 
     @on(ListView.Selected, '#list-pager')
     async def on_selected_list(self, event: ListView.Selected) -> None:
+        event.list_view.loading = True
         ep: BaseEpisode = event.item.value
+
         sources = await self.extractor.a_get_sources(ep)
+
         if not sources:
             self.notify('Not found (maybe episode not released yet?)')
+            event.item.loading = False
             return
 
+        event.list_view.loading = False
         await self.push_screen(SourceResultScreen(sources))
 
     @on(ListView.Selected, '#source-items')
     async def get_videos(self, event: ListView.Selected):
+        event.list_view.loading = True
         source: BaseSource = event.item.value
+
         videos = await self.extractor.a_get_videos(source)
-        self.notify(f'picked {source}')
+
+        event.list_view.loading = False
         await self.push_screen(VideoResultScreen(videos))
 
     @on(Button.Pressed, '.back-source, .back-search, .back-anime, .back-video')
@@ -122,8 +147,16 @@ class AnicliRuTui(App):
     @on(ListView.Selected, '#video-items')
     async def play_video(self, event: ListView.Selected):
         video = event.item.value
+        event.list_view.loading = True
+
         self.notify(f'run video {video}')
-        run_video(video, player='mpv')
+
+        self._play_video(video)
+        event.list_view.loading = False
+
+    @work(thread=True)
+    def _play_video(self, video):
+        run_video(video)
 
     def action_open_page(self, url: str):
         webbrowser.open(url)
@@ -131,6 +164,11 @@ class AnicliRuTui(App):
     def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.dark = not self.dark
+
+    def action_pop_screen(self):
+        if len(self.screen_stack) == 1:
+            return
+        self.pop_screen()
 
 
 if __name__ == '__main__':
