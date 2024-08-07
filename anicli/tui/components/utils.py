f651bb96f238ba91c30b75f1c75bdeb1af3fb0b3
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-08-07 16:43:46 +0300

    add update list_view function

--- anicli/tui/components/utils.py
+++ anicli/tui/components/utils.py
@@ -2,15 +2,26 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from textual.widget import Widget
+    from textual.widgets import ListView, ListItem
 
 from contextlib import contextmanager
 
 
 @contextmanager
-def start_loading(element: 'Widget'):
+def set_loading(*elements: 'Widget') -> None:
     """helper context manager that starts loading element state"""
     try:
-        element.loading = True
-        yield element
+        for el in elements:
+            el.loading = True
+        yield
+
     finally:
-        element.loading = False
+        for el in elements:
+            el.loading = False
+
+
+def update_list_view(list_view: 'ListView', *items: 'ListItem') -> 'ListView':
+    """clear current list_view widget and set as items values"""
+    list_view.clear()
+    list_view.extend(items)
+    return list_view
