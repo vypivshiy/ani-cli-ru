46d0a346732f8b79b4b0e6609180b036e2728a1e
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-08-07 16:45:13 +0300

    add utils function

--- anicli/tui/components/__init__.py
+++ anicli/tui/components/__init__.py
@@ -1,2 +1,3 @@
 from .paginator import AnimeListItem, ListPaginator, new_list_paginator, new_list_view
 from .widgets import ButtonPopScreen, AppHeader, MiddleTitle
+from .utils import set_loading, update_list_view
