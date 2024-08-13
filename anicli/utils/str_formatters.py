8264d74f32c4ac23bdae3b0b596708d95fddc4ac
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-08-13 21:18:27 +0300

    add netloc function

--- anicli/utils/str_formatters.py
+++ anicli/utils/str_formatters.py
@@ -1,3 +1,6 @@
+from urllib.parse import urlsplit
+
+
 def float_to_hms(duration_float: float) -> str:
     """convert float or integet value to human-readable string (HH:MM:SS format)"""
     total_seconds = int(round(duration_float))
@@ -7,3 +10,7 @@ def float_to_hms(duration_float: float) -> str:
     seconds = total_seconds % 60
 
     return f"{hours:02}:{minutes:02}:{seconds:02}"
+
+
+def netloc(url: str) -> str:
+    return urlsplit(url).netloc
