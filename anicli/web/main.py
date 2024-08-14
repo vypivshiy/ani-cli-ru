0a96e7c662eb0f7c00749549f107cf0840b0646b
georgiy <59173419+vypivshiy@users.noreply.github.com>
2024-08-14 17:23:03 +0300

    first dummy prototype web interface

---
+++ anicli/web/main.py
@@ -0,0 +1,84 @@
+from anicli_api.source.animego import Extractor
+from anicli_api.base import BaseSource
+from flask import Flask, render_template, request
+
+from ..utils.cached_extractor import CachedExtractor, CachedItemContext
+from ..utils.fetch_extractors import get_extractor_modules
+
+app = Flask(__name__)
+
+EX = CachedExtractor(Extractor())
+CONTEXT = CachedItemContext(extractor=EX)
+
+
+# {ongoing: {}, search: {}, anime: {}, episodes: {}, sources: {}, video: {} }
+
+@app.get('/')
+def main():
+    modules = get_extractor_modules()
+
+    ongoings_results = CONTEXT.ongoing()
+    ongoings_element = render_template('ongoings_block.j2', ongoings_results=ongoings_results)
+    select_providers = render_template('extractor_providers_block.j2', modules=modules)
+
+    return render_template('index.j2',
+                           select_providers=select_providers,
+                           ongoings=ongoings_element
+                           )
+
+
+@app.get('/search')
+def search():
+    # TODO: validate
+    query = request.args.get('query', '')
+
+    searches = CONTEXT.search(query)
+    return render_template('search.j2', search_results=searches, title=f"Search {query}")
+
+
+@app.post('/anime')
+def anime_page():
+    title = request.form['title']
+    url = request.form['url']
+
+    # HACK: get search/ongoing from cache by title and url
+    match_item = [o for o in CONTEXT.searches_or_ongoings if o.title == title and o.url == url][0]
+    anime = CONTEXT.get_anime(match_item)
+    episodes = CONTEXT.get_episodes()
+    return render_template('anime.j2', title=anime.title, episodes=episodes, anime=anime)
+
+
+@app.get('/get_sources')
+def anime_sources():
+    num = request.args.get('num').strip()
+    ep = [e for e in CONTEXT.episodes if e.num == num][0]
+    sources = CONTEXT.get_sources(ep)
+    return render_template('anime_source_block.j2', sources=sources)
+
+
+@app.get('/get_videos')
+def anime_videos():
+    url = request.args.get('url').strip()
+    title = request.args.get('title').strip()
+
+    source = [s for s in CONTEXT.sources if s.title == title and s.url == url][0]
+    CONTEXT.picked_source = source
+
+    videos = CONTEXT.get_videos(source)
+    return render_template('anime_videos_block.j2', videos=videos)
+
+
+@app.get('/spawn_player')
+def anime_spawn_player():
+    # '{"quality": "{{ video.quality }}", "type": "{{ video.type }}", "url": "{{ video.url }}"}'
+    url = request.args.get('url').strip()
+    quality = int(request.args.get('quality').strip())
+    type_ = request.args.get('type').strip()
+    CONTEXT.picked_source
+    video = [v for v in CONTEXT.videos if v.url == url and v.quality==quality and v.type == type_][0]
+    CONTEXT.picked_video = video
+
+    return render_template('anime_player_js_block.j2', video=video)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ anicli/web/static/player.js
@@ -0,0 +1,62 @@
+function playerJS(url, headers = {}) {
+    const videoContainer = document.getElementById('video-container');
+    videoContainer.innerHTML = ''; // Clear previous video
+
+    // Create the video element
+    const video = document.createElement('video');
+    video.controls = true;
+    video.style.width = '100%';
+    videoContainer.appendChild(video);
+
+    // Check the video URL to determine the format
+    const extension = url.split('.').pop().toLowerCase();
+
+    if (extension === 'mp4') {
+        // MP4 video
+        video.src = url;
+    } else if (extension === 'm3u8') {
+        // HLS (M3U8) stream
+        if (Hls.isSupported()) {
+            const hls = new Hls();
+            hls.loadSource(url);
+            hls.attachMedia(video);
+            hls.on(Hls.Events.MANIFEST_PARSED, () => video.play());
+        } else {
+            video.src = url;
+            video.addEventListener('loadedmetadata', () => video.play());
+        }
+    } else if (extension === 'mpd') {
+        // DASH (MPD) stream
+        if (dashjs.MediaPlayer.isSupported()) {
+            const player = dashjs.MediaPlayer().create();
+            player.initialize(video, url, true);
+        } else {
+            console.error('DASH.js not supported');
+        }
+    } else {
+        console.error('Unsupported video format');
+        return;
+    }
+
+    // Set custom headers if required
+    if (Object.keys(headers).length > 0) {
+        const videoSource = video.src;
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', videoSource, true);
+
+        for (const [key, value] of Object.entries(headers)) {
+            xhr.setRequestHeader(key, value);
+        }
+
+        xhr.onload = function() {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                video.src = URL.createObjectURL(xhr.response);
+            } else {
+                console.error('Failed to load video with custom headers');
+            }
+        };
+
+        xhr.responseType = 'blob';
+        xhr.send();
+    }
+}
