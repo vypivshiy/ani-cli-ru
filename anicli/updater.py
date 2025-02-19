2a207ae7c7e36694b47e8574d0ac3dc3cbcd35f0
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-02-19 19:12:08 +0300

    add upgrade client call add --quiet flags
    uv:
    add extra calls upgrade pacakge

--- anicli/updater.py
+++ anicli/updater.py
@@ -1,21 +1,29 @@
 import subprocess
 
+
 def _check_installed_cli_package(cmd: str, cli_package: str="anicli-ru") -> bool:
     proc = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     return proc.returncode == 0 and cli_package in proc.stdout
 
+
 def is_installed_in_pipx() -> bool:
     return _check_installed_cli_package("pipx list")
+
 
 def is_installed_in_uv():
     return _check_installed_cli_package("uv tool list")
 
 
 def update_pipx():
-    # double calls:
-    # first - update pypi cache, second - update package
-    subprocess.run("pipx runpip anicli-ru install anicli-api -U", shell=True, stdout=subprocess.PIPE)
-    subprocess.run("pipx runpip anicli-ru install anicli-api -U", shell=True)
+    subprocess.run("pipx upgrade anicli-ru -q", shell=True)
+
+    # double calls required for update pip cache and guaranteed get actual version
+    subprocess.run("pipx runpip anicli-ru install anicli-api -U -q", shell=True)
+    subprocess.run("pipx runpip anicli-ru install anicli-api -U -q", shell=True)
+
 
 def update_uv():
-    subprocess.run("uv tool upgrade anicli-ru", shell=True)
+    subprocess.run("uv tool upgrade anicli-ru -q", shell=True)
+
+    subprocess.run("uv tool upgrade anicli-ru --upgrade-package anicli-api -q", shell=True)
+    subprocess.run("uv tool upgrade anicli-ru --upgrade-package anicli-api -q", shell=True)
