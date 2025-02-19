5b0a6d83e97b0ac24ab674000a40d2c163d95a07
Georgiy <59173419+vypivshiy@users.noreply.github.com>
2025-02-19 19:12:34 +0300

    add check client version updates

--- anicli/cli/events.py
+++ anicli/cli/events.py
@@ -21,9 +21,12 @@ def loaded_extractor_msg():
     app.cmd.print_ft("Loaded source provider:", urlsplit(app.CFG.EXTRACTOR.BASE_URL).netloc)
 
 @app.on_startup()
-def check_anicli_api_version():
-    result, current, new = check_version()
-    if result:
-        print()
-        print(f"available new anicli-api version: (current: {current}, new: {new})")
-        print("close app and run `anicli-ru --update` to update anicli-api package")
+def check_updates():
+    result_api, current_api, new_api = check_version()
+    result_client, current_client, new_client = check_version("anicli-ru")
+    if result_api:
+        print(f"available new anicli-api version: (current: {current_api}, new: {new_api})")
+    elif result_client:
+        print(f"available new anicli-ru version: (current: {current_api}, new: {new_api})")
+    if result_api or result_client:
+        print("close app and run `anicli-ru -U` to get updates")
