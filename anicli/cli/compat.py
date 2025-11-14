1a84f6bb9e0f9f6f0b7dcddde76dce9e40ed2c27
georgiy <vypivshiy@shitposting.expert>
2025-11-14 20:29:23 +0300

    remove all old code

--- anicli/cli/compat.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if sys.version_info >= (3, 11):
-    import tomllib
-else:
-    import toml as tomllib
-
-__all__ = ["tomllib"]
--- anicli/headers.py
+++
@@ -1,6 +0,0 @@
-from typing import Tuple
-
-
-def parse_header_line(line: str) -> Tuple[str, str]:
-    key, value = line.split('=', 1)
-    return key, value
